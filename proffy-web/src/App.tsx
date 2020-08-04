aa7d62c97396f66a85140db42b89c279903ea63d
Guezin <leandroguezinjunior@gmail.com>
2020-08-04 19:34:17 -0300

    paginas Landing, TeacherForm, TeacherList e components PageHeader, TeahcerItem criados.

--- proffy-web/src/App.tsx
+++ proffy-web/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './routes';
 
+import './styles/global.css';
+
 const App: React.FC = () => (
   <Router>
     <Routes />
---
+++ proffy-web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS1DeyZNqRdLF9WiyJOo7YQW5HxbSp3F6tNQQ&usqp=CAU"
+          alt="Imagem Avatar"
+        />
+
+        <div>
+          <strong>Leandro Guezin Jr</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
