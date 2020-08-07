8ce21f553bc83b745261fdc88f4712fd601b3e5c
Guezin <leandroguezinjunior@gmail.com>
2020-08-07 19:56:18 -0300

    conectando com a API, carregando informacoes total conexoes

--- proffy-web/src/pages/Landing/index.tsx
+++ proffy-web/src/pages/Landing/index.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import logoImg from '../../assets/images/logo.svg';
-import ladingImg from '../../assets/images/landing.svg';
+import api from "../../services/api";
 
-import studyIcon from '../../assets/images/icons/study.svg';
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import logoImg from "../../assets/images/logo.svg";
+import ladingImg from "../../assets/images/landing.svg";
 
-import './styles.css';
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+import "./styles.css";
+
+interface IResponse {
+  total: number;
+}
 
 const Home: React.FC = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    const loadConnections = async () => {
+      const { data } = await api.get<IResponse>("/connections");
+
+      setTotalConnections(data.total);
+    };
+
+    loadConnections();
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-
         <div className="logo-container">
-          <img src={logoImg} alt="Proff"/>
+          <img src={logoImg} alt="Proff" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
 
@@ -28,23 +45,23 @@ const Home: React.FC = () => {
 
         <div className="buttons-container">
           <Link to="/study" className="study">
-            <img src={studyIcon} alt="Estudar"/>
+            <img src={studyIcon} alt="Estudar" />
             Estudar
           </Link>
 
           <Link to="/give-classes" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar aula"/>
-            Dar aula
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
           </Link>
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
-          <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
