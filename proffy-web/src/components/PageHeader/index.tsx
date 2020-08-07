b2b892c5b29ab41f2f676264400c366f5c0a3fa9
Guezin <leandroguezinjunior@gmail.com>
2020-08-07 18:54:50 -0300

    components/ Inpu, Textarea criados

--- proffy-web/src/components/PageHeader/index.tsx
+++ proffy-web/src/components/PageHeader/index.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import backIcon from '../../assets/images/icons/back.svg';
-import logoImg from '../../assets/images/logo.svg';
+import backIcon from "../../assets/images/icons/back.svg";
+import logoImg from "../../assets/images/logo.svg";
 
-import './styles.css';
+import "./styles.css";
 
 interface IPageHeaderProps {
   title: string;
+  description?: string;
 }
 
-const PageHeader: React.FC<IPageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<IPageHeaderProps> = ({
+  title,
+  description,
+  children,
+}) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
         <Link to="/">
-          <img src={backIcon} alt="Voltar"/>
+          <img src={backIcon} alt="Voltar" />
         </Link>
 
-        <img src={logoImg} alt="Proffy"/>
+        <img src={logoImg} alt="Proffy" />
       </div>
 
       <div className="header-content">
         <strong>{title}</strong>
 
+        {description && <p>{description}</p>}
+
         {children}
       </div>
-
     </header>
   );
 };
